8f5cfd11f5ebc0ddd47ec55437a1667e4f9f0ea2
Nikita <nikitamartynenko555@gmail.com>
2024-04-11 23:16:24 +0300

    header and footer

--- about.php
+++ about.php
@@ -1,37 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arsenal Football Club</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/accardion.css">
-    <link rel="stylesheet" href="css/back.css">
-    <link rel="stylesheet" href="css/contact.css">
-    <link rel="stylesheet" href="css/gallery.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-</head>
-<body>
-    <header class="container main-header">
-        <div>
-          <a href="index.html">
-            <img src="img/logo.png" height="40" alt="Arsenal FC">
-          </a>
-        </div>
-        <nav class="main-nav">
-          <ul class="main-menu" id="main-menu">
-              <li><a href="index.html">Home</a></li>
-              <li><a href="about.html">About us</a></li>
-              <li><a href="question.html">Question</a></li>
-              <li><a href="contact.html">Contact</a></li>
-          </ul>
-          <a class="hamburger" id="hamburger">
-              <i class="fa fa-bars"></i>
-          </a>
-        </nav>
-    </header>
+<?php
+    include_once('partials/header.php');
+?>
     <main class="gallery">
         
         <section class="imggalr">
@@ -58,44 +27,6 @@
             </div>
     </main>
 
-    <footer class="footer">
-        <div class="container-footer">
-            <div class="row">
-                <div class="footer-col">
-                    <h4>company</h4>
-                    <ul>
-                        <li><a href="index.html">home</a></li>
-                        <li><a href="about.html">about us</a></li>
-                        <li><a href="contact.html">contact us</a></li>
-                        <li><a href="question.html">question</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>get help</h4>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">shipping</a></li>
-                        <li><a href="#">returns</a></li>
-                        <li><a href="#">order now</a></li>
-                        <li><a href="#">payment options</a></li>
-                    </ul>
-                </div>
-                <div class="footer-col">
-                    <h4>online shop arsenal</h4>
-                    <ul>
-                        <li><a href="#">watch</a></li>
-                        <li><a href="#">bag</a></li>
-                        <li><a href="#">shoes</a></li>
-                        <li><a href="#">dress</a></li>
-                    </ul>
-                </div>
-                
-
-            </div>
-        </div>
-
-    </footer>
-    <script src="js/main.js"></script>
-    <script src="js/accardion.js"></script>
-</body>
-</html>
+<?php
+    include_once('partials/footer.php');
+?>
---
+++ partials/footer.php
@@ -0,0 +1,45 @@
+<?php
+    require_once('inc/function.php'); 
+?>
+<footer class="footer">
+        <div class="container-footer">
+            <div class="row">
+                <div class="footer-col">
+                    <h4>company</h4>
+                    <ul>
+                        <li><a href="index.php">home</a></li>
+                        <li><a href="about.php">about us</a></li>
+                        <li><a href="contact.php">contact us</a></li>
+                        <li><a href="question.php">question</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>get help</h4>
+                    <ul>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">shipping</a></li>
+                        <li><a href="#">returns</a></li>
+                        <li><a href="#">order now</a></li>
+                        <li><a href="#">payment options</a></li>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h4>online shop arsenal</h4>
+                    <ul>
+                        <li><a href="#">watch</a></li>
+                        <li><a href="#">bag</a></li>
+                        <li><a href="#">shoes</a></li>
+                        <li><a href="#">dress</a></li>
+                    </ul>
+                </div>
+                
+
+            </div>
+        </div>
+
+    </footer>
+    <?php
+        add_scripts();
+    ?>
+</body>
+</html>
